5f18cfd6e619fd98b1118caa118f269b736613a5
Gvulture <rejwan150015@gmail.com>
2026-07-25 12:51:55 +0600

    jj

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/OTPScreenController.java
@@ -0,0 +1,22 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class OTPScreenController
+{
+    @javafx.fxml.FXML
+    private TextField enterOTPTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void verifiesOTPOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backToForgotIDPasswordOnClick(ActionEvent actionEvent) {
+    }
+}
