acaa30960169d9f3722903082af2251ae178690a
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-02 00:32:06 +0600

    Completed wedding

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/CompletedWeddingController.java
@@ -0,0 +1,18 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+
+import javafx.scene.control.TableColumn;
+
+public class CompletedWeddingController
+{
+    @javafx.fxml.FXML
+    private TableColumn ClientTC;
+    @javafx.fxml.FXML
+    private TableColumn StatusTC;
+    @javafx.fxml.FXML
+    private TableColumn WeddingIDTC;
+    @javafx.fxml.FXML
+    private TableColumn DateTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
