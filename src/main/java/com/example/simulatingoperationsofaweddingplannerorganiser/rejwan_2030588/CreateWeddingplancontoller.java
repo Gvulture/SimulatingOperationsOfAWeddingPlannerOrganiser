60039301fe3c1240abdfdf49d3802a168331fe04
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-01 21:20:28 +0600

    CreateWeddingplan

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/CreateWeddingplancontoller.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/CreateWeddingplancontoller.java
@@ -1,7 +1,34 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class CreateWeddingplancontoller
 {
     @javafx.fxml.FXML
+    private ComboBox ThemeCombobox;
+    @javafx.fxml.FXML
+    private TextField BudgetTextField;
+    @javafx.fxml.FXML
+    private TextField ClientNameTextField;
+    @javafx.fxml.FXML
+    private DatePicker WeddingDateDatePicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void ResetOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SaveOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BacktoWPDashbordOnClick(ActionEvent actionEvent) {
+    }
+}
