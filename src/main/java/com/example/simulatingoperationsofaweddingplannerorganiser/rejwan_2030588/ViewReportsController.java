3605758411ffbcc3dcaee9562c10c14b0f73865c
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-01 22:30:21 +0600

    ViewReport

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/ViewReportsController.java
@@ -0,0 +1,12 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+
+import javafx.scene.control.ComboBox;
+
+public class ViewReportsController
+{
+    @javafx.fxml.FXML
+    private ComboBox ReportTyperComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
