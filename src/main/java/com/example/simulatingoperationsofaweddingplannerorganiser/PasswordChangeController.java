dae8d18d5af049f8f01bfa6d1fea5ddbe6e1f92c
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-01 20:40:54 +0600

    passward change

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/PasswordChangeController.java
@@ -0,0 +1,12 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser;
+
+public class PasswordChangeController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backToLoginOnClick(ActionEvent actionEvent) {
+    }
+}
