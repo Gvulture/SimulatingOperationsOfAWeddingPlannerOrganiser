1f36b58e2c20175f42a25837db5b0f65ed521054
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-01 23:14:00 +0600

    Manage guest

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/ManageGuest.java
@@ -0,0 +1,14 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+
+import javafx.scene.control.TextField;
+
+public class ManageGuest
+{
+    @javafx.fxml.FXML
+    private TextField GuestNameTextfield;
+    @javafx.fxml.FXML
+    private TextField PhoneTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
