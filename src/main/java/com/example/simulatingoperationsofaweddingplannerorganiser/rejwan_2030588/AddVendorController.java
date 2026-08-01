7f317f67d7627bd760158bb86a09d2a0c0a5f486
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-02 01:09:25 +0600

    Add vendor

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/AddVendorController.java
@@ -0,0 +1,28 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class AddVendorController
+{
+    @javafx.fxml.FXML
+    private TextField PriceTextField;
+    @javafx.fxml.FXML
+    private TableColumn PrinceTC;
+    @javafx.fxml.FXML
+    private TableColumn PhoneTC;
+    @javafx.fxml.FXML
+    private TextField PhoneTextField;
+    @javafx.fxml.FXML
+    private TableColumn VendorNameTC;
+    @javafx.fxml.FXML
+    private TableColumn ServiceTypeTC;
+    @javafx.fxml.FXML
+    private ComboBox ServiceTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField VendorNameTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
