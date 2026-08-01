34e3efffec61ad776a6b05a14ec5bb36474419df
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-02 02:28:32 +0600

    Vendor Payment

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/VendorPaymentController.java
@@ -0,0 +1,24 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class VendorPaymentController
+{
+    @javafx.fxml.FXML
+    private TableColumn PaymentMethodTC;
+    @javafx.fxml.FXML
+    private TableColumn VendorTC;
+    @javafx.fxml.FXML
+    private TextField AmountField;
+    @javafx.fxml.FXML
+    private TableColumn AmountTC;
+    @javafx.fxml.FXML
+    private ComboBox PaymentMethodComboBox;
+    @javafx.fxml.FXML
+    private ComboBox VendorComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
