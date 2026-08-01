fb936f753ac8d67b40228ec49806b6869ea5996c
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-02 02:04:44 +0600

    Approve booking

---
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/ApproveBookingController.java
@@ -0,0 +1,12 @@
+package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+
+import javafx.scene.control.TableColumn;
+
+public class ApproveBookingController
+{
+    @javafx.fxml.FXML
+    private TableColumn BookingListTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
