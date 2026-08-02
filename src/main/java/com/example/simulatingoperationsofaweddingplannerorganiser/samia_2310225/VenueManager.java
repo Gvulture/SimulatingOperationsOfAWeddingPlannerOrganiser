cf6575fb351c67d96b018f93b71b027788d431af
Samia Tasmim Tithi <samiatithy35@gmail.com>
2026-08-02 20:13:21 +0600

    completed venue manager to inherit user

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/samia_2310225/VenueManager.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/samia_2310225/VenueManager.java
@@ -1,4 +1,38 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.samia_2310225;
 
-public class VenueManager {
+import com.example.simulatingoperationsofaweddingplannerorganiser.common.User;
+
+public class VenueManager extends User {
+
+    public VenueManager(int id, String name, String role, int contact, String email, String password) {
+        super(id, name, role, contact, email, password);
+    }
+
+    public void addVenue() {
+
+    }
+
+    public void updateVenue() {
+
+    }
+
+    public void viewBookingRequests() {
+
+    }
+
+    public void updateBookingStatus() {
+
+    }
+
+    public void viewSchedule() {
+
+    }
+
+    public void viewPaymentStatus() {
+
+    }
+
+    public void viewAllClients() {
+
+    }
 }
