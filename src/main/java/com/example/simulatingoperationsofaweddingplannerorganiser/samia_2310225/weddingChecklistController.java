054ab6559ffc88c50014d44749ea95a5a3eb5fbe
Samia Tasmim Tithi <samiatithy35@gmail.com>
2026-08-02 15:11:41 +0600

    completed all fx-id and on action to independent classes

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/samia_2310225/weddingChecklistController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/samia_2310225/weddingChecklistController.java
@@ -1,7 +1,35 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.samia_2310225;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class weddingChecklistController
 {
     @javafx.fxml.FXML
+    private TableColumn statusTableColumn;
+    @javafx.fxml.FXML
+    private Label showMessageLabel;
+    @javafx.fxml.FXML
+    private TableColumn deadlineTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn taskNameTableColumn;
+    @javafx.fxml.FXML
+    private TableView weddingChecklistTableView;
+    @javafx.fxml.FXML
+    private ComboBox selectTaskComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleMarkAsDoneOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackToDashboard(ActionEvent actionEvent) {
+    }
+}
