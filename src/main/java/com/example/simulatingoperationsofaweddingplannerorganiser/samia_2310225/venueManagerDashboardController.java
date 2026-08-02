4f612892061c99aa483714e5f9ae61134375e3e4
Samia Tasmim Tithi <samiatithy35@gmail.com>
2026-08-02 17:41:20 +0600

    completed both dashboard

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/samia_2310225/venueManagerDashboardController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/samia_2310225/venueManagerDashboardController.java
@@ -1,7 +1,46 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.samia_2310225;
 
+import javafx.event.ActionEvent;
+
 public class venueManagerDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleUpdateBookingStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewAllClientsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewScheduleOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleUpdateVenueOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAddNewVenueOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handlePaymentStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAcceptRejectOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogOutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewBookingRequestOnAction(ActionEvent actionEvent) {
+    }
+}
