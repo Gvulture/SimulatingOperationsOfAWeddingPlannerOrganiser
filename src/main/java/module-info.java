ba56a01f1d3b5c39591672eacbc76be1c25b409a
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-02 15:46:33 +0600

    Guest dash

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module com.example.simulatingoperationsofaweddingplannerorganiser {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.simulatingoperationsofaweddingplannerorganiser to javafx.fxml;
     exports com.example.simulatingoperationsofaweddingplannerorganiser;
+
+    opens com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588 to javafx.fxml;
+    exports com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
+    opens com.example.simulatingoperationsofaweddingplannerorganiser.samia_2310225 to javafx.fxml;
+    exports com.example.simulatingoperationsofaweddingplannerorganiser.samia_2310225;
+    opens com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777 to javafx.fxml;
+    exports com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777;
 }
