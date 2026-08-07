38941cc889babd919f85544a8c41c8223047dda7
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 02:48:40 +0600

    systemlog completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/AccountantDashboard.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/AccountantDashboard.java
@@ -1,4 +1,8 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
 public class AccountantDashboard {
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
 }
--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/AdministratorDashboard.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/AdministratorDashboard.java
@@ -1,4 +1,7 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
 public class AdministratorDashboard {
+    @javafx.fxml.FXML
+      public void initialize() {
+}
 }
