1b0700f57fa305d7479f1d58958b30957ee9ad56
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 19:35:08 +0600

    budgetmanagemntcontrollter completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/BudgetManagementController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/BudgetManagementController.java
@@ -1,4 +1,21 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class BudgetManagementController {
+    @FXML
+    private TextField txtRemainingBudget;
+
+    @FXML
+    private TextField txtBudgetStatus;
+
+    @FXML
+    public void initialize() {
+        txtRemainingBudget.setText("0.00");
+        txtBudgetStatus.setText("Not Verified");
+
+    }
+
 }
