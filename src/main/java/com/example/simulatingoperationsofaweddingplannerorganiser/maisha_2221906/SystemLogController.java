fe1149fe9060f9efe5665bf9a90328563817fdc2
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 21:08:52 +0600

    systemlogcontroller completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/SystemLogController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/SystemLogController.java
@@ -1,4 +1,31 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class SystemLogController {
+    @FXML
+    private ComboBox<String> activityTypeComboBox;
+
+    @FXML
+    private ComboBox<String> userRoleComboBox;
+
+    @FXML
+    public void initialize() {
+
+        activityTypeComboBox.getItems().addAll(
+                "Login",
+                "Logout",
+                "Create",
+                "Update",
+                "Delete",
+                "View"
+        );
+
+        userRoleComboBox.getItems().addAll(
+                "Admin",
+                "Accountant",
+                "Planner"
+        );
+    }
 }
