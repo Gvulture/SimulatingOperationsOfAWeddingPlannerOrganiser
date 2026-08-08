e7c6ff810b65b93936d2bbf7e91b9f250efcdbab
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 21:13:57 +0600

    systemreportcontroller completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/SystemReportController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/SystemReportController.java
@@ -1,4 +1,38 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class SystemReportController {
+    @FXML
+    private ComboBox<String> reportTypeComboBox;
+
+    @FXML
+    private ComboBox<String> userRoleComboBox;
+
+    @FXML
+    private ComboBox<String> reportStatusComboBox;
+
+    @FXML
+    public void initialize() {
+
+        reportTypeComboBox.getItems().addAll(
+                "User Report",
+                "Activity Report",
+                "System Report",
+                "Financial Report"
+        );
+
+        userRoleComboBox.getItems().addAll(
+                "Admin",
+                "Accountant",
+                "Planner"
+        );
+
+        reportStatusComboBox.getItems().addAll(
+                "Generated",
+                "Pending",
+                "Failed"
+        );
+    }
 }
