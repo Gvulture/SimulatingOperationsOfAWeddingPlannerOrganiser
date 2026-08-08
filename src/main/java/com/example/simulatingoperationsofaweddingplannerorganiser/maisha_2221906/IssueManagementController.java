f61b70cb663e2f786282dcf26265a1a680e6d85f
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 20:43:35 +0600

    issueManagementcontroller completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/IssueManagementController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/IssueManagementController.java
@@ -1,4 +1,49 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class IssueManagementController {
+    @FXML
+    private ComboBox<String> issueCategoryComboBox;
+
+    @FXML
+    private ComboBox<String> priorityComboBox;
+
+    @FXML
+    private ComboBox<String> issueStatusComboBox;
+
+
+    @FXML
+    public void initialize() {
+
+
+        issueCategoryComboBox.getItems().addAll(
+                "Technical",
+                "Database",
+                "Security",
+                "User Account",
+                "Payment",
+                "Other"
+        );
+
+
+        priorityComboBox.getItems().addAll(
+                "Low",
+                "Medium",
+                "High",
+                "Critical"
+        );
+
+
+        issueStatusComboBox.getItems().addAll(
+                "Open",
+                "In Progress",
+                "Resolved",
+                "Closed"
+        );
+
+
+        issueStatusComboBox.setValue("Open");
+    }
 }
