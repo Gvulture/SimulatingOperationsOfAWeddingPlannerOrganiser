28691fd1e7d83cb79da5e38080636c5bd79846dc
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-08 13:00:22 +0600

    minor fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -19,9 +19,4 @@ module com.example.simulatingoperationsofaweddingplannerorganiser {
     opens com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777 to javafx.fxml;
     exports com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777;
 
-    exports com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777.NonUser;
-    opens com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777.NonUser to javafx.fxml;
-
-    exports com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777.User;
-    opens com.example.simulatingoperationsofaweddingplannerorganiser.Jhalok_2411777.User to javafx.fxml;
 }
