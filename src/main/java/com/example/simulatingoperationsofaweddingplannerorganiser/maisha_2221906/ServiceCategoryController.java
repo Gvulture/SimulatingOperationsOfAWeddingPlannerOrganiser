22cbad9e7ce101ea217cfdf05e7856f476d3f2a8
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 21:03:47 +0600

    servicecategorycontroller completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/ServiceCategoryController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/ServiceCategoryController.java
@@ -1,4 +1,32 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class ServiceCategoryController {
-}
+
+        @FXML
+        private ComboBox<String> serviceTypeComboBox;
+
+        @FXML
+        private ComboBox<String> statusComboBox;
+
+        @FXML
+        public void initialize() {
+
+            serviceTypeComboBox.getItems().addAll(
+                    "Catering",
+                    "Decoration",
+                    "Photography",
+                    "Venue",
+                    "Music",
+                    "Transportation"
+            );
+
+            statusComboBox.getItems().addAll(
+                    "Active",
+                    "Inactive"
+            );
+        }
+    }
+
