607929c823c61cba8707839b44db9b95961be54b
Rejwan Ahammed <rejwan150015@gmail.com>
2026-08-08 14:39:41 +0600

    VendorDAshboard code fix

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/VendorDashboardController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/rejwan_2030588/VendorDashboardController.java
@@ -1,7 +1,56 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.rejwan_2030588;
 
+import com.example.simulatingoperationsofaweddingplannerorganiser.SceneSwitcher;
+import javafx.event.ActionEvent;
+
 public class VendorDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void VendorPerformanceOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/VendorPerformance.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void AvailabilityOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/VendorAvailability.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void AddVendorOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/AddVendor.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateVendorOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/UpdateVendor.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void ViewBookingOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/ViewBookings.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void ApproveBookingOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/ApproveBooking.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void VendorPaymentOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/VendorPayment.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateReportOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"rejwan_2030588/ViewReports.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void LogoutOA(ActionEvent actionEvent) {
+        SceneSwitcher.switchTo(actionEvent,"Login.fxml");
+    }
+}
