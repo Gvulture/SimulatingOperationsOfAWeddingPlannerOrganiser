b44a239ccb5321b13c8701a975a303f6d0450d58
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 20:53:02 +0600

    paymentcontroller completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/PaymentController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/PaymentController.java
@@ -1,4 +1,22 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class PaymentController {
+
+    @FXML
+    private ComboBox<String> paymentMethodComboBox;
+
+    @FXML
+    public void initialize() {
+
+        paymentMethodComboBox.getItems().addAll(
+                "Cash",
+                "Bank Transfer",
+                "Credit Card",
+                "Debit Card",
+                "Mobile Banking"
+        );
+    }
 }
