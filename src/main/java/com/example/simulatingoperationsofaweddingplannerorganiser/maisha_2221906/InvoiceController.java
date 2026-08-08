f12abc8471298fdf87329032f3f44632b5bef1f8
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 20:16:50 +0600

    invoiceController completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/InvoiceController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/InvoiceController.java
@@ -1,4 +1,20 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class InvoiceController {
+
+    @FXML
+    private ComboBox<String> paymentStatusComboBox;
+
+    @FXML
+    public void initialize() {
+        paymentStatusComboBox.getItems().addAll(
+                "Pending",
+                "Paid",
+                "Partially Paid",
+                "Overdue"
+        );
+    }
 }
