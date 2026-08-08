8b21a8d0a426bfe2589f089dca3170166e4f994d
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 20:57:53 +0600

    securitycontroller completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/SecurityController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/SecurityController.java
@@ -1,4 +1,30 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class SecurityController {
+    @FXML
+    private ComboBox<String> userRoleComboBox;
+
+    @FXML
+    private ComboBox<String> accountStatusComboBox;
+
+    @FXML
+    public void initialize() {
+
+        // User Role options
+        userRoleComboBox.getItems().addAll(
+                "Admin",
+                "Accountant",
+                "Planner"
+        );
+
+        // Account Status options
+        accountStatusComboBox.getItems().addAll(
+                "Active",
+                "Locked",
+                "Inactive"
+        );
+    }
 }
