fea78867b5ed11abc47f5c52ae7b080b9542fa47
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 19:54:00 +0600

    FinancialReportController completed

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/FinancialReportController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/FinancialReportController.java
@@ -1,4 +1,28 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class FinancialReportController {
+    @FXML
+    private ComboBox<String> cmbReportType;
+
+    @FXML
+    private TextArea statusornotificatinTextarea;
+
+    @FXML
+    public void initialize() {
+
+        cmbReportType.getItems().addAll(
+                "Income Report",
+                "Expense Report",
+                "Payment Report",
+                "Outstanding Balance Report",
+                "Budget Report",
+                "Financial Summary"
+        );
+
+        statusornotificatinTextarea.setText("Ready to generate report.");
+    }
 }
