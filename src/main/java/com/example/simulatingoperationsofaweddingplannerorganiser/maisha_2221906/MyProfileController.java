cd3c565cb806f0167b9fdd6446582fb7b5072d38
Mahjabin.maisha <mahjabinmimmi@gmail.com>
2026-08-08 20:49:16 +0600

    My profile controller

--- src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/MyProfileController.java
+++ src/main/java/com/example/simulatingoperationsofaweddingplannerorganiser/maisha_2221906/MyProfileController.java
@@ -1,4 +1,19 @@
 package com.example.simulatingoperationsofaweddingplannerorganiser.maisha_2221906;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class MyProfileController {
+    @FXML
+    private ComboBox<String> roleComboBox;
+
+    @FXML
+    public void initialize() {
+
+        roleComboBox.getItems().addAll(
+                "Accountant",
+                "Administrator"
+        );
+
+    }
 }
